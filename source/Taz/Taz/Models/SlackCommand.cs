e828e3931bd2c81e6a3131a600f65e352df31660
Yohan Belval <yohan.belval@gsoft.com>
2016-06-04 12:12:41 -0400

    Slack command api endpoint

---
+++ source/Taz/Taz/Models/SlackCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace Taz.Models
+{
+    public class SlackCommand
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("team_id")]
+        public string TeamId { get; set; }
+
+        [JsonProperty("team_domain")]
+        public string TeamDomain { get; set; }
+
+        [JsonProperty("channel_id")]
+        public string ChannelId { get; set; }
+
+        [JsonProperty("channel_name")]
+        public string ChannelName { get; set; }
+
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("user_name")]
+        public string UserName { get; set; }
+
+        [JsonProperty("command")]
+        public string Command { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("response_url")]
+        public Uri ResponseUrl { get; set; }
+    }
+}
