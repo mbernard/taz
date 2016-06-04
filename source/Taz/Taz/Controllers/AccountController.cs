eba9696a91dfbb19ad68ab3fd78a7a53e085b4b8
Miguel Bernard <mbernard@sybersphere.com>
2016-06-04 11:01:18 -0400

    fix

---
+++ source/Taz/Taz/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SlackAPI;
+
+namespace Taz.Controllers
+{
+    public class AccountController : Controller
+    {
+        public string SignIn(string code)
+        {
+            SlackClient.GetAccessToken(this.OnResponse, "48203572272.48200981078", "ab29427778b4fb9d8ea6c59aae50a8db", null, code);
+
+            return "bot token:";
+        }
+
+        private void OnResponse(AccessTokenResponse accessTokenResponse)
+        {
+            var accessToken = accessTokenResponse.access_token;
+        }
+    }
+}
