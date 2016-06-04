90ecb4392de8c2aa4187c75b3dab2a129ad17064
Miguel Bernard <mbernard@sybersphere.com>
2016-06-04 12:18:20 -0400

    ajout du token factory

---
+++ source/Taz/Taz.Core/SlackClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SlackAPI;
+
+namespace Taz.Core
+{
+    public static class SlackClientFactory
+    {
+        public static SlackClient CreateClient(User user)
+        {
+            var token = TokenLoader.GetTokenFor(user);
+            return new SlackClient(token);
+        }
+    }
+}
