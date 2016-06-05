185dd608794425beb72e7a776eb5a44a9c4a1324
Philippe Lavoie <Philippe.Lavoie@gsoft.com>
2016-06-05 03:09:14 -0400

    [Landing Page] DONE ! _dropping mic_

---
+++ source/Taz/Taz/Content/js/app.js
@@ -0,0 +1,20 @@
+﻿
+
+$(document).ready(function () {
+    $(".carousel").owlCarousel({
+
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        autoPlay: true
+
+        // "singleItem:true" is a shortcut for:
+        // items : 1, 
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
+});
